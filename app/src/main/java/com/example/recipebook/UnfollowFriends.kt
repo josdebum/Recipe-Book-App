0540e1d2ae3395c3ac354fae84c8fabad85021ec
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2020-04-30 19:40:10 +0100

    new implementations

---
+++ app/src/main/java/com/example/recipebook/UnfollowFriends.kt
@@ -0,0 +1,12 @@
+package com.example.recipebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UnfollowFriends : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unfollow_friends)
+    }
+}
