25e04306e4955cf9cd963ac5351dc5bbb58be41b
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2020-11-04 14:12:33 +0100

    Create User implementation

--- app/src/main/java/com/example/recipebook/CreateAccountActivity.kt
+++ app/src/main/java/com/example/recipebook/CreateAccountActivity.kt
@@ -1,89 +1,175 @@
 package com.example.recipebook
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_create_account.*
+import kotlinx.android.synthetic.main.activity_welcome_screen.*
 
 
 class CreateAccountActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+    private val TAG = "CreateAccountActivity"
+    private var etEmail: EditText? = null
+    private var etPassword: EditText? = null
+    private var btnCreateAccount: Button? = null
+    private var mProgressBar: ProgressBar? = null
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var etFirstName: EditText? = null
+    private var etLastName: EditText? = null
+
+    private var email: String? = null
+    private var password: String? = null
+    private var firstName: String? = null
+    private var lastName: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        initialise()
 
 
-        val one: String = Email.text.toString().trim()
-        val two: String = password.text.toString().trim()
-        val PhoneNumber = findViewById<EditText>(R.id.phoneNumber)
-        val CreateAccount = findViewById<Button>(R.id.create_button)
-        val ProgressBar = findViewById<ProgressBar>(R.id.progress_bar)
-        val fAuth = FirebaseAuth.getInstance()
+//        mAuth = FirebaseAuth.getInstance()
+//
+//
+//
+//
+//        create_button.setOnClickListener() {
+//
+//            var addEmail = input_email.text.toString().trim()
+//            var addPassword = input_password.text.toString().trim()
+//
+//
+//            if (TextUtils.isEmpty(Email.text.toString())){
+//                Email?.error = ("Email is required.")
+//            }
+//            if (TextUtils.isEmpty(password.text.toString())){
+//                password?.error = ("Password is required!")
+//
+//            }
+//            if (password.length() <6){
+//                password?.error = ("Password must be >= 6 characters!")
+//
+//            }
+//
+//
+//
+//            mAuth!!.createUserWithEmailAndPassword( addEmail,addPassword).addOnCompleteListener(
+//                this
+//            ) { task: Task<AuthResult> ->
+//                if (task.isSuccessful) {
+//                    var user: FirebaseUser = mAuth!!.currentUser!!
+//                    Log.d("User", user.email.toString())
+//
+//                    Toast.makeText(this, "User has been created.", Toast.LENGTH_LONG).show()
+//
+//                       startActivity(Intent(this, Home::class.java))
+//                } else {
+//                    Log.d("Error", task.toString())
+//
+//                    Toast.makeText(this, "Error has occurred", Toast.LENGTH_LONG).show()
+//                }
+//
+//
+//            }
+//
+//        }
+//    }
+    }
+
+    private fun initialise() {
+
+        firstName = etFirstName?.text.toString()
+        lastName = etLastName?.text.toString()
 
 
+        etEmail = findViewById<View>(R.id.email) as EditText
+        etPassword = findViewById<View>(R.id.password) as EditText
+        btnCreateAccount = findViewById<View>(R.id.btnCreateAccount) as Button
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        mProgressBar = findViewById<ProgressBar>(R.id.progress_bar);
+        btnCreateAccount!!.setOnClickListener { createNewAccount() }
+    }
+    private fun createNewAccount() {
 
+        email = etEmail?.text.toString()
+        password = etPassword?.text.toString()
 
-        create_button.setOnClickListener(View.OnClickListener() {
-            @Override
-            fun onClick(view: TextView) {
+        if ( !TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName)
+            && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgressBar!!.visibility = View.VISIBLE
 
+            mAuth!!
+                .createUserWithEmailAndPassword(email!!, password!!)
+                .addOnCompleteListener(this) { task ->
+                    mProgressBar!!.visibility = View.INVISIBLE
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("users" , "createUserWithEmail:success")
+                        val userId = mAuth!!.currentUser!!.uid
 
+                        startActivity(Intent(this, Home::class.java))
+                        //Verify Email
+                        //verifyEmail();
+                        //update user profile information
+                        val currentUserDb = mDatabaseReference!!.child(userId)
+                        currentUserDb.child("firstName").setValue(firstName)
+                        currentUserDb.child("lastName").setValue(lastName)
 
-            if (TextUtils.isEmpty(Email.text.toString())){
-                Email?.error = ("Email is required.")
-            }
-            if (TextUtils.isEmpty(password.text.toString())){
-                password?.error = ("Password is required!")
-
-            }
-            if (password.length() <6){
-                password?.error = ("Password must be >= 6 characters!")
-
-            }
-            progress_bar.visibility = View.VISIBLE
-
-            FirebaseAuth.getInstance()
-                .createUserWithEmailAndPassword(one, two).addOnCompleteListener(this) { task: Task<AuthResult> ->
-                    @Override
-                    fun onComplete(@NonNull task: Task<AuthResult> ){
-
-                    if (task.isSuccessful()){
-                        Toast.makeText(this, "User has been created.", Toast.LENGTH_LONG).show()
-
-                       startActivity(Intent(this, Home::class.java))
+                        updateUserInfoAndUI()
                     } else {
-                        Toast.makeText(this, "Error has occurred", Toast.LENGTH_LONG).show()
-
-
+                        // If sign in fails, display a message to the user.
+                        Log.d("Error", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this@CreateAccountActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
                     }
-
-
                 }
 
 
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
         }
 
+    }
 
+    private fun updateUserInfoAndUI() {
+        //start next activity
+        val intent = Intent(this@CreateAccountActivity, Home::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
 
-
+//    private fun verifyEmail() {
+//        val mUser = mAuth!!.currentUser;
+//        mUser!!.sendEmailVerification()
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    Toast.makeText(this@CreateAccountActivity,
+//                        "Verification email sent to " + mUser.getEmail(),
+//                        Toast.LENGTH_SHORT).show()
+//                } else {
+//                    Log.d("users", "sendEmailVerification", task.exception)
+//                    Toast.makeText(this@CreateAccountActivity,
+//                        "Failed to send verification email.",
+//                        Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//    }
 }
-
-            if (fAuth.currentUser != null) {
-                startActivity(Intent(applicationContext, Home::class.java))
-
-            }
-        })
-
-
-        fun String.isEmailValid() = android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
-
-// Usage
-        "example@gmail.com".isEmailValid() // Return true
-        "example@!?!?.com".isEmailValid() // Return false
-    }}
--- app/src/main/java/com/example/recipebook/WelcomeScreen.kt
+++ app/src/main/java/com/example/recipebook/WelcomeScreen.kt
@@ -3,17 +3,20 @@ package com.example.recipebook
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_welcome_screen.*
 
 
 class WelcomeScreen : AppCompatActivity() {
-
+    private var mAuth: FirebaseAuth? = null
+    private var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,22 +26,43 @@ class WelcomeScreen : AppCompatActivity() {
             startActivity(Intent(this, CreateAccountActivity::class.java))
         }
 
-        val email = "example@gmail.com"
-        val password = "demoPassword"
-        FirebaseAuth.getInstance()
-            .signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, { task: Task<AuthResult> ->
+
+        mAuth = FirebaseAuth.getInstance()
 
 
-            })
+        login.setOnClickListener() {
 
+            var inputEmail = input_email.text.toString().trim()
+            var inputPassword = input_password.text.toString().trim()
 
-        fun isLoggedIn() = FirebaseAuth.getInstance().currentUser != null
-
-        if (isLoggedIn()) {
-            // Show logout page
-        } else {
-            // Show login page
+//                mAuth!!.signInWithEmailAndPassword(inputEmail, inputPassword).
+//        addOnCompleteListener { task:Task<AuthResult> ->
+            if (inputEmail == "josephdeborah506@gmail.com" && inputPassword == "josdebum") {
+                startActivity(Intent(this, Home::class.java))
+            } else {
+                Toast.makeText(this, "Signed in not successful", Toast.LENGTH_LONG).show()
+            }
         }
+
+
+        // startActivity(Intent(this, MainScreen::class.java))
     }
 }
+
+
+//    override fun onStart (){
+//        super.onStart ()
+//        currentUser = mAuth!!.currentUser
+//
+//
+//
+//        if (currentUser != null){
+//            Toast.makeText(this, "User is logged in", Toast.LENGTH_LONG).show()
+//        }
+//        else {
+//            Toast.makeText(this, "User is logged out", Toast.LENGTH_LONG).show()
+//        }}}
+
+//}
+
+
